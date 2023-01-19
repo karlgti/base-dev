9a0105770c54c684d18e95a950ce34a40e33cf3a
prakashrajpurohit <59438098+prakashrajpurohit@users.noreply.github.com>
2023-01-19 15:37:46 +0530

    Update index.ts

--- http/index.ts
+++ http/index.ts
@@ -1,12 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-
-    baseURL: 'https://base-strapi.onrender.com',
+  baseURL: "https://base-strapi.onrender.com",
 });
 
+const api2 = axios.create({
+  baseURL: "https://api.twitter.com/2",
+  headers: {
+    Authorization: `Bearer ${"AAAAAAAAAAAAAAAAAAAAAGualAEAAAAA4HcksQD7%2FnV8W3ztoWAPTcH2V4M%3DAeQmNKoz3hk6cSRD52xPxyCFXkAqDcVo6F949BNsqx01VmRhot"}`,
+  },
+});
 
+export const fetchArticle = async () =>
+  api2.get(
+    `/users/2840576778/tweets?max_results=5&tweet.fields=created_at&expansions=attachments.media_keys&media.fields=preview_image_url,url`
+  );
 
 //Insight Articles
 export const fetchArticles = async () =>
-    api.get(`/api/insights?populate=*&sort[0]=blog_id`);
+  api.get(`/api/insights?populate=*&sort[0]=blog_id`);
